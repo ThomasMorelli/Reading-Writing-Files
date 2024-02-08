15a60615ab6a936581491253d296019fe8924753
ThomasMorelli <thomas_morelli1@baylor.edu>
2024-02-08 11:09:52 -0600

    EmployeePay Finished

--- EmployeePay.py
+++ EmployeePay.py
@@ -4,13 +4,16 @@ employees = open('employee_data.csv','r')
 
 csv_file = csv.reader(employees)
 
+
+
 #TO SKIP THE HEADER
 next(csv_file)
 
 for rec in csv_file:
-    total_pay = float(rec[7]) * int(rec[3])
-    print(f"Name:   ${rec[1]}")
+    total_pay = (float(rec[7]) + 1 ) * (int(rec[3]))
+    bonus = (float(rec[7]) + 1 ) * (int(rec[3])) - (int(rec[3]))
+    print(f"Name:   {rec[1]}")
     print(f"Salary: ${rec[3]}")
-    print(f"Bonus:  ${rec[7]}")
+    print(f"Bonus:  ${bonus}")
     print(f"Pay:    ${total_pay}")
     input()
